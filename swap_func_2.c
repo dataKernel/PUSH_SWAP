afd4560d9cb217b14f8559bde6deb084ebdb5522
dataKernel <lancelot.saintleger@gmail.com>
2023-06-20 18:06:00 +0100

    fix: testing new rotate via nodes directly

--- swap_func_2.c
+++ swap_func_2.c
@@ -18,21 +18,13 @@ t_node	*pop(t_node **head)
 void    rotate(t_node **head)
 {
     t_node  *oldHead;
-    int     firstElem;
+    t_node  *position;
 
     oldHead = *head;
-    firstElem = (*head)->value;
-    while(*head != NULL)
-    {
-        if((*head)->next == NULL)
-        {
-            (*head)->value = firstElem;
-            *head = oldHead;
-            return ;
-        }
-        (*head)->value = (*head)->next->value;
-        *head = (*head)->next;
-    }
+    *head = (*head)->next;
+    position = *head;
+
+    printf("old:%i__head:%i__posi:%i", oldHead->value, (*head)->value, position->value);
 }
 
 void    rotate_all(t_node **headA, t_node **headB)
