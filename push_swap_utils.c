259ba0020f0118a48bd72a0b6e53ef4bcd018b26
dataKernel <lancelot.saintleger@gmail.com>
2023-07-08 18:47:40 +0100

    ff

--- push_swap_utils.c
+++ push_swap_utils.c
@@ -43,10 +43,6 @@ void	sort_list(t_node **headA, int actualIndex, int insertIndex)
 
 	rotateResult = check_nbr_rotate(*headA, actualIndex, insertIndex);
 	reverseResult = check_nbr_reverse(*headA, actualIndex, insertIndex);
-	if (rotateResult < 0)
-		rotateResult += size_list(*headA);
-	if (reverseResult < 0)
-		reverseResult += size_list(*headA);
 	if (rotateResult < reverseResult)
 	{
 		while (rotateResult > 0)
